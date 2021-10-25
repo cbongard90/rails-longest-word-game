79ce1de63c4f7e3e22bd0199d659e1eb09d55b7e
Christian Bongard <christian.bongard90@gmail.com>
2021-10-25 17:47:52 +0400

    Refactored the answers

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -16,13 +16,13 @@ class GamesController < ApplicationController
     guess = params[:guess].upcase
     grid = params[:letters].split(',')
 
-    valid_answer = valid_answer?(guess, grid)
-    valid_word = valid_word?(guess)
-    if valid_answer && valid_word
-      win_message(guess)
-    else
-      lose_message(guess, valid_word)
-    end
+    @valid_answer = valid_answer?(guess, grid)
+    @valid_word = valid_word?(guess)
+    # if valid_answer && valid_word
+    #   @answer = win_message()
+    # else
+    #   @answer = lose_message(guess, valid_word)
+    # end
   end
 
   private
@@ -44,23 +44,5 @@ class GamesController < ApplicationController
     word_serialized = URI.open(url).read
     word = JSON.parse(word_serialized)
     word['found']
-  end
-
-  def win_message
-    {
-      message: 'Well Done!'
-    }
-  end
-
-  def lose_message(guess)
-    if valid_word
-      {
-        message: "The word #{guess} is not in the grid"
-      }
-    else
-      {
-        message: "The word #{guess} is not an english word"
-      }
-    end
   end
 end
